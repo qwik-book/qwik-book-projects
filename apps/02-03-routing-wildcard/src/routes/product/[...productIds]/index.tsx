2c0b843fcb42930c1a974d9fbf92d7021f00b65b
anartzdev <105705996+anartzdev@users.noreply.github.com>
2023-09-01 07:08:43 +0200

    feat(chapter-02)

---
+++ apps/02-03-routing-wildcard/src/routes/product/[...productIds]/index.tsx
@@ -0,0 +1,70 @@
+import { component$ } from '@builder.io/qwik';
+import { type DocumentHead, useLocation } from '@builder.io/qwik-city';
+
+export default component$(() => {
+  const location = useLocation();
+  return (
+    <>
+      <div>Product Item [...productIds] - Principal Page</div>
+      <p>Href: {location.url.href}</p>
+      <p>Pathname: {location.url.pathname}</p>
+      <p>Product Id:</p>{' '}
+      <ul>
+        {location.params.productIds.split('/').map((id, index) => {
+            return (id !== '') ? (
+                <li>
+                  Product select ({index + 1}): {id}
+                </li> 
+              ): undefined
+        })}
+      </ul>
+    </>
+  );
+});
+
+export const head: DocumentHead = {
+    title: "Qwik Book - Proyectos - Rutas comodín - Wildcard",
+    meta: [
+      {
+        name: "description",
+        content:
+          "Trabajando con los diferentes proyectos del libro de Qwik en Español",
+      },
+      {
+        name: "keywords",
+        content: "qwik, qwik-book",
+      },
+      {
+        name: "author",
+        content: "Anartz Mugika Ledo",
+      },
+      {
+        name: "og:image",
+        content:
+          "https://jgengle.github.io/Leaflet/examples/quick-start/thumbnail.png",
+      },
+      {
+        name: "og:url",
+        content: "https://github.com/qwik-book/qwik-book-projects",
+      },
+      {
+        name: "twitter:image",
+        content:
+          "https://jgengle.github.io/Leaflet/examples/quick-start/thumbnail.png",
+      },
+      {
+        name: "twitter:card",
+        content: "summary_large_image",
+      },
+      {
+        name: "twitter:title",
+        content: "Qwik - El libro",
+      },
+      {
+        name: "twitter:description",
+        content:
+          "Aprende Qwik desde 0 paso a paso aplicando conceptos teórico-prácticas hasta publicar nuestros proyectos",
+      },
+    ],
+};
+  
